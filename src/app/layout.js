449c1454dad6136a123e494404ccd8c6d18761b1
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-05-27 23:10:59 -0300

    navbar terminado y funcionando

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,10 +13,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-<Head>
-  <link rel="icon" href="./favicon.ico" />
-</Head>
-      <body className={inter.className}>{children}</body>
+      <Head>
+        <link rel="icon" href="./favicon.ico" />
+      </Head>
+      <body
+        className={`flex h-screen flex-col lg:flex-row md:overflow-hidden ${inter.className}`}
+      >
+        <div className="w-full lg:w-64">
+          <Navbar />
+        </div>
+        <div className="flex-grow p-6 lg:overflow-y-auto lg:p-12">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex flex-row lg:flex-col lg:h-screen justify-center items-start lg:space-y-4 space-x-4 lg:space-x-0 p-4">
+      <Link href="/about">
+        <p className="hover:text-gray-300 uppercase text-center block md:w-auto w-full">
+          About
+        </p>
+      </Link>
+      <Link href="/projects">
+        <p className="hover:text-gray-300 uppercase text-center block md:w-auto w-full">
+          Proyectos
+        </p>
+      </Link>
+      <Link href="/skills">
+        <p className="hover:text-gray-300 uppercase text-center block md:w-auto w-full">
+          Skills
+        </p>
+      </Link>
+      <Link href="/contact">
+        <p className="hover:text-gray-300 uppercase text-center block md:w-auto w-full">
+          Contacto
+        </p>
+      </Link>
+    </nav>
+  );
+}
